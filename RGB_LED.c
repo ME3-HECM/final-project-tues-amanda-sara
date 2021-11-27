1f84e666a987dc4d68a52dd2693cffba72d7ec0e
ssaf-khan <92157938+ssaf-khan@users.noreply.github.com>
2021-11-27 11:40:32 +0000

    RGB functions

---
+++ RGB_LED.c
@@ -0,0 +1,33 @@
+/*
+ * File:   RGB_LED.c
+ * Author: s
+ *
+ * Created on November 27, 2021, 11:31 AM
+ */
+
+
+#include <xc.h>
+#include "RGB_LED.h"
+
+void RGB_init(void) {
+    TRISGbits.TRISG1 = 0; //red LED
+    TRISAbits.TRISA4 = 0; //green LED
+    TRISFbits.TRISF7 = 0; //blue LED
+    
+    LATGbits.LATG1 = 0;
+    LATAbits.LATA4 = 0;
+    LATFbits.LATF7 = 0;
+    
+}
+
+void redLED(void){
+    LATGbits.LATG1 = !LATGbits.LATG1;
+}
+
+void greenLED(void){
+    LATAbits.LATA4 = !LATAbits.LATA4;
+}
+
+void blueLED(void){
+    LATFbits.LATF7 = !LATFbits.LATF7;
+}
---
+++ RGB_LED.h
@@ -0,0 +1,13 @@
+
+#ifndef _RGB_LED_H
+#define	_RGB_LED_H
+
+#include <xc.h>   
+
+void RGB_LED(void);
+void redLED(void);
+void greenLED(void);
+void blueLED(void);
+
+#endif
+
