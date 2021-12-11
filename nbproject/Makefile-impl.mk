5270527941e53d0dd74b9f6086e8f3a73d584756
Amanda Mei Wah Lee <amanda.lee19@imperial.ac.uk>
2021-12-11 14:02:57 +0000

    Fixed makefile errors (on amanda's pc)

--- nbproject/Makefile-impl.mk
+++ nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=final-project-tues-amanda-sara.X
+PROJECTNAME=final-project-tues-amanda-sara-v2.X
 
 # Active Configuration
 DEFAULTCONF=default
