1fbe2b74bf0b29c8172b90e650a6d264bf29317a
Amanda Mei Wah Lee <amanda.lee19@imperial.ac.uk>
2021-12-11 13:20:34 +0000

    Reading cards attempt#2 (also added an excel spreadsheet for colour card RGB readings)

--- color_card.c
+++ color_card.c
@@ -1,30 +1,60 @@
 #include <xc.h>
+#include "color_card.h"
 #include "color_click.h"
+#include "dc_motor.h"
 
-RGB_val read_card(RGB_val current) {
-    redLED = 1;
-    current.R = colorclick_readRed();
-    __delay_ms(1000);
-    redLED = 0;
-    __delay_ms(20);
+void read_card(RGB_val initial, RGB_val current, DC_motor *mL, DC_motor *mR) {
+    // At 5 cm distance
+    unsigned int R_rel = current.R/current.C;
+    unsigned int G_rel = current.G/current.C;
+    unsigned int B_rel = current.B/current.C;
     
-    greenLED = 1;
-    current.G = colorclick_readGreen();
-    __delay_ms(1000);
-    greenLED = 0;
-    __delay_ms(20);
-    
-    blueLED = 1;
-    current.B = colorclick_readBlue();
-    __delay_ms(1000);
-    blueLED = 0;
-    __delay_ms(20);
-    
-    return current;
-}
-
-void check_red(unsigned int initial, unsigned int current) {
-    unsigned int diff;
-    diff = current - initial;
-    if(diff>150) LATHbits.LATH3 = !LATHbits.LATH3;
+    if ((R_rel>0.54) && (G_rel<0.245) && (B_rel<0.18)) {
+        // Red card - Turn right 90 degrees
+        turnRight(mL, mR, 90);
+        stop(mL, mR);
+        
+    } else if ((R_rel<0.435) && (G_rel>0.31) && (B_rel>0.195)) {
+        // Green card - Turn left 90 degrees
+        turnLeft(mL, mR, 90);
+        stop(mL, mR);
+        
+    } else if ((R_rel<0.43) && (G_rel>0.30) && (B_rel>0.21)) {
+        // Blue card - Turn 180 degrees
+        turnRight(mL, mR, 180);
+        stop(mL, mR);
+        
+    } else if ((R_rel>0.49) && (G_rel>0.285) && (B_rel>0.18)) {
+        // Yellow card - Reverse 1 square and turn right 90 degrees
+        reverse(mL, mR);
+        turnRight(mL, mR, 90);
+        stop(mL, mR);
+        
+    } else if ((R_rel>0.49) && (G_rel<0.275) && (B_rel>0.195)) {
+        // Pink card - Reverse 1 square and turn left 90 degrees
+        reverse(mL, mR);
+        turnLeft(mL, mR, 90);
+        stop(mL, mR);
+        
+    } else if ((R_rel>0.54) && (G_rel<0.24) && (B_rel<0.18)) {
+        // Orange card - Turn right 135 degrees
+        turnRight(mL, mR, 135);
+        stop(mL, mR);
+        
+    } else if ((R_rel<0.44) && (G_rel>0.305) && (B_rel>0.21)) {
+        // Light blue card - Turn left 135 degrees
+        turnLeft(mL, mR, 135);
+        stop(mL, mR);
+        
+    } else if ((R_rel<0.46) && (G_rel>0.295) && (B_rel>0.21)) {
+        // White card - Finish (return home)
+        turnRight(mL, mR, 180);
+        stop(mL, mR);
+        
+    } else {
+        // Exceptions (return back to the starting position if final card cannot be found)
+        returnhome_flag = 1;
+        turnRight(mL, mR, 180);
+        stop(mL, mR);
+    }
 }
--- color_card.h
+++ color_card.h
@@ -3,9 +3,13 @@
 
 #include <xc.h>
 #include "color_click.h"
+#include "dc_motor.h"
 
 #define _XTAL_FREQ 64000000 //note intrinsic _delay function is 62.5ns at 64,000,000Hz  
 
-RGB_val read_card(RGB_val current);
+volatile unsigned char returnhome_flag;
+
+RGB_val read_colour(RGB_val current);
+void read_card(RGB_val initial, RGB_val current, DC_motor *mL, DC_motor *mR);
 
 #endif
